be427be1bbaab484a58c126a42878e9ecb6d07e7
Paul Wood <paul@Paul-Woods-iMac.local>
2012-09-18 12:26:29 +0800

    images are being uploaded and can be viewed

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 TimelapseServer
 ===============
 
-The server used to work the Timelapse Live app, it pushes the pictures to varioud social networks
+The server used to work the Timelapse Live app, it pushes the pictures to various social networks
+
+Goals
+=====
+
+Server host a small website where you can see a chronological list of the pictures placed on the site.
+
+When a picture is uploaded it has a comment and parameters for each social network. Parameters are "immediate" or "queued". Immediate pushes the image to the social network immediately and queued adds the image to the queue for that social network. Each social network has a time and an interval to place the image.
+
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
-var express = require('express');
-
+var express = require('express')
+  , format = require('util').format;
+    
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.use(express.bodyParser({ keepExtensions: true, uploadDir: 'static/images' }));
+app.use(express.static(__dirname + '/static'));
+app.use(express.errorHandler({
+   dumpExceptions: true, 
+   showStack: true
+ }));
+ 
+app.get('/', function(request, res) {
+  res.send('<form method="post" enctype="multipart/form-data">'
+    + '<p>Title: <input type="text" name="title" /></p>'
+    + '<p>Image: <input type="file" name="image" /></p>'
+    + '<p><input type="submit" value="Upload" /></p>'
+    + '</form>');
 });
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+app.post('/', function(req, res, next){
+  // the uploaded file can be found as `req.files.image` and the
+  // title field as `req.body.title`
+  res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+    , req.files.image.name
+    , req.files.image.size / 1024 | 0 
+    , req.files.image.path
+    , req.body.title));
 });
