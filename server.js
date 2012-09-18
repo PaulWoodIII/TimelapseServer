12e820b51e176eff96532fab0f326a781789a323
Paul Wood <paul@Paul-Woods-iMac.local>
2012-09-18 12:55:45 +0800

    working with cloudinary now

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express')
   , format = require('util').format;
+var fs = require('fs');
+var cloudinary = require('cloudinary');
     
 var app = express.createServer(express.logger());
-app.use(express.bodyParser({ keepExtensions: true, uploadDir: 'static/images' }));
+app.use(express.bodyParser());
 app.use(express.static(__dirname + '/static'));
 app.use(express.errorHandler({
    dumpExceptions: true, 
@@ -10,10 +12,15 @@ app.use(express.errorHandler({
  }));
  
 app.get('/', function(request, res) {
+  // res.send('<form method="post" enctype="multipart/form-data">'
+  //   + '<p>Title: <input type="text" name="title" /></p>'
+  //   + '<p>Image: <input type="file" name="image" /></p>'
+  //   + '<p><input type="submit" value="Upload" /></p>'
+  //   + '</form>');
   res.send('<form method="post" enctype="multipart/form-data">'
-    + '<p>Title: <input type="text" name="title" /></p>'
-    + '<p>Image: <input type="file" name="image" /></p>'
-    + '<p><input type="submit" value="Upload" /></p>'
+    + '<p>Public ID: <input type="text" name="title"/></p>'
+    + '<p>Image: <input type="file" name="image"/></p>'
+    + '<p><input type="submit" value="Upload"/></p>'
     + '</form>');
 });
 
@@ -26,9 +33,17 @@ app.listen(port, function() {
 app.post('/', function(req, res, next){
   // the uploaded file can be found as `req.files.image` and the
   // title field as `req.body.title`
-  res.send(format('\nuploaded %s (%d Kb) to %s as %s'
-    , req.files.image.name
-    , req.files.image.size / 1024 | 0 
-    , req.files.image.path
-    , req.body.title));
+  // res.send(format('\nuploaded %s (%d Kb) to %s as %s'
+  //   , req.files.image.name
+  //   , req.files.image.size / 1024 | 0 
+  //   , req.files.image.path
+  //   , req.body.title));
+  
+  stream = cloudinary.uploader.upload_stream(function(result) {
+    console.log(result);
+    res.send('Done:<br/> <img src="' + result.url + '"/><br/>' + 
+             cloudinary.image(result.public_id, { format: "png", width: 100, height: 130, crop: "fill" }));
+  }, { public_id: req.body.title } );
+  fs.createReadStream(req.files.image.path, {encoding: 'binary'}).on('data', stream.write).on('end', stream.end);
+  
 });
